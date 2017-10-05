733edb1b3ebd0e947916869988cf381812772917
饗庭　秀一郎 <syuichiroaiba@aebashuuichirounoMacBook-Pro.local>
2017-10-05 19:27:41 +0900

    4 http req

--- index.js
+++ index.js
@@ -4,8 +4,9 @@
  * @param {object} event The Cloud Functions event.
  * @param {function} callback The callback function.
  */
-exports.helloWorld = function helloWorld (event, callback) {
-  console.log(`My Cloud Function: ${event.body.message}`);
+exports.helloWorld = function helloWorld (req, res) {
+  console.log(`My Cloud Function: ${req.body.message}`);
+  res.status(200).send('Success : ' + req.body.message);
   callback();
 };
 
