f0724b798c756c35e52b244ef66496a27a3fe8be
饗庭　秀一郎 <syuichiroaiba@aebashuuichirounoMacBook-Pro.local>
2017-10-05 19:33:09 +0900

    Fix console log

--- index.js
+++ index.js
@@ -5,7 +5,7 @@
  * @param {function} callback The callback function.
  */
 exports.helloWorld = function helloWorld (req, res) {
-  console.log(`My Cloud Function: ${req.body.message}`);
+  console.log(req.body.message);
   res.status(200).send('Success : ' + req.body.message);
 };
 
