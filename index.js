83815593cbe617a8c5cd5d6990103fab9483cbb5
饗庭　秀一郎 <syuichiroaiba@aebashuuichirounoMacBook-Pro.local>
2017-10-05 19:41:00 +0900

    Fix typo

--- index.js
+++ index.js
@@ -4,7 +4,7 @@
  * @param {object} event The Cloud Functions event.
  * @param {function} callback The callback function.
  */
-exports.helloWorld = function helloWorld (req, res) {
+exports.helloWorld = function helloWorld(req, res) {
   console.log(req.body['message']);
   res.status(200).send('Success : ' + req.body['message']);
 };
