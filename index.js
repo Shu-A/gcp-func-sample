ab5f74fdd63c1b9bf0534aa178b16b8c45b12c77
饗庭　秀一郎 <syuichiroaiba@aebashuuichirounoMacBook-Pro.local>
2017-10-05 19:57:03 +0900

    Fix to sample

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 /**
  * Cloud Function.
  *
- * @param {object} event The Cloud Functions event.
- * @param {function} callback The callback function.
+ * @param {!Object} req Cloud Function request context.
+ * @param {!Object} res Cloud Function response context.
  */
 exports.helloWorld = function helloWorld(req, res) {
-  console.log(req.body['message']);
-  res.status(200).send('Success : ' + req.body['message']);
+  console.log(req.body.message);
+  res.status(200).send('Success : ' + req.body.message);
 };
 
